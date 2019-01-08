260e60ab19ca154b3cd1b8c72d0a8190cd7fa49e
Alexey Shipin <alexey.shipin@gmail.com>
2019-01-08 22:58:59 +0300

    added new tickets page

--- src/Service/UserTicket/GetList.php
+++ src/Service/UserTicket/GetList.php
@@ -17,74 +17,87 @@ use Doctrine\ORM\EntityManager;
 
 class GetList
 {
-    const INTERVAL = "P7D";
+	const INTERVAL = "P7D";
 
-    /**
-     * @var EntityManager
-     */
-    private $entityManager;
-    /**
-     * @var \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository
-     */
-    private $userTicketRepository;
-    /**
-     * @var UserTicketsProcessor
-     */
-    private $userTicketsProcessor;
+	/**
+	 * @var EntityManager
+	 */
+	private $entityManager;
+	/**
+	 * @var \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository
+	 */
+	private $userTicketRepository;
+	/**
+	 * @var UserTicketsProcessor
+	 */
+	private $userTicketsProcessor;
 
-    public function __construct(
-        EntityManager $entityManager,
-        UserTicketsProcessor $userTicketsProcessor
-    ) {
+	public function __construct(
+		EntityManager $entityManager,
+		UserTicketsProcessor $userTicketsProcessor
+	) {
 
-        $this->entityManager = $entityManager;
-        $this->userTicketRepository = $entityManager->getRepository(UserTicket::class);
-        $this->userTicketsProcessor = $userTicketsProcessor;
-    }
+		$this->entityManager = $entityManager;
+		$this->userTicketRepository = $entityManager->getRepository(UserTicket::class);
+		$this->userTicketsProcessor = $userTicketsProcessor;
+	}
 
-    private function get(): Collection
-    {
-        $userTickets = $this->userTicketRepository->matching(
-            Criteria::create()
-        );
+	private function get(): Collection
+	{
+		$userTickets = $this->userTicketRepository->matching(
+			Criteria::create()->orderBy(['id' => 'DESC'])
+		);
 
-        return $userTickets;
-    }
+		return $userTickets;
+	}
 
-    /**
-     * @return Collection
-     * @throws \Exception
-     */
-    public function getExpirationUserTickets(): Collection
-    {
-        $userTickets = $this->get();
+	/**
+	 * @return Collection
+	 */
+	public function getNewTickets(): Collection
+	{
+		$userTickets = $this->get();
 
-        /** @var UserTicket[] $userTicketsArray */
-        $userTicketsArray = $userTickets->toArray();
+		/** @var UserTicket[] $userTicketsArray */
+		$userTicketsArray = $userTickets->toArray();
 
-        usort(
-            $userTicketsArray, function (UserTicket $a, UserTicket $b) {
-            return ($a->getExpirationDate() < $b->getExpirationDate()) ? -1 : 1;
-        }
-        );
+		return new ArrayCollection($userTicketsArray);
+	}
 
-        foreach ($userTicketsArray as $key => $userTicket) {
-            if ($userTicket->getTicketPlan()->getType()->getId() == 3) {
-                unset($userTicketsArray[$key]);
-            }
-            if ($userTicket->getExpirationDate() < (new \DateTime())->sub(new \DateInterval(self::INTERVAL))) {
-                unset($userTicketsArray[$key]);
-            }
-            if ($userTicket->getExpirationDate() > (new \DateTime())->add(new \DateInterval(self::INTERVAL))) {
-                unset($userTicketsArray[$key]);
-            }
-            if ($this->userTicketsProcessor->userTicketSum($userTicket->getUser()->getUserTickets())
-                > $userTicket->getLessonsExpires()
-            ) {
-                unset($userTicketsArray[$key]);
-            }
-        }
+	/**
+	 * @return Collection
+	 * @throws \Exception
+	 */
+	public function getExpirationUserTickets(): Collection
+	{
+		$userTickets = $this->get();
 
-        return new ArrayCollection($userTicketsArray);
-    }
+		/** @var UserTicket[] $userTicketsArray */
+		$userTicketsArray = $userTickets->toArray();
+
+		usort(
+			$userTicketsArray, function (UserTicket $a, UserTicket $b) {
+			return ($a->getExpirationDate() < $b->getExpirationDate()) ? -1 : 1;
+		}
+		);
+
+		foreach ($userTicketsArray as $key => $userTicket) {
+			if ($userTicket->getTicketPlan()->getType()->getId() == 3) {
+				unset($userTicketsArray[$key]);
+			}
+			if ($userTicket->getExpirationDate() < (new \DateTime())->sub(new \DateInterval(self::INTERVAL))) {
+				unset($userTicketsArray[$key]);
+			}
+			if ($userTicket->getExpirationDate() > (new \DateTime())->add(new \DateInterval(self::INTERVAL))) {
+				unset($userTicketsArray[$key]);
+			}
+			if ($this->userTicketsProcessor->userTicketSum($userTicket->getUser()->getUserTickets())
+				> $userTicket->getLessonsExpires()
+			) {
+				unset($userTicketsArray[$key]);
+			}
+		}
+
+		return new ArrayCollection($userTicketsArray);
+	}
 }
